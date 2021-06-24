502138817c5bf92e0a012f4fee095b0874717606
Rendy <rendyrn047@gmail.com>
2021-06-25 01:22:09 +0700

    dump: merlin_id_global_images_V12.0.1.0.QJOIDXM_20200807.0000.00_10.0_global_b1a2750067.tgz

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-Link="https://index.rnxyr.workers.dev/miui_MERLIN_V12.0.8.0.QJOCNXM_468d5166c9_10.0.zip"
+Link="https://index.rnxyr.workers.dev/merlin_id_global_images_V12.0.1.0.QJOIDXM_20200807.0000.00_10.0_global_b1a2750067.tgz"
 FileName="$(pwd)/miui_MERLINGlobal_V12.0.1.0.RJOMIXM_9bf35d329b_11.0.zip"
 export DEBIAN_FRONTEND=noninteractive
 apt-get -y update && apt-get -y upgrade && apt-get install -y unace unrar zip unzip p7zip-full p7zip-rar sharutils rar uudeview mpack arj cabextract file-roller device-tree-compiler liblzma-dev python3 python3-pip brotli liblz4-tool axel gawk aria2 detox wget
