d09d7bf83d864ff9eab7ba34d31eb5e174032217
Rendy <rendyrn047@gmail.com>
2021-07-28 12:32:58 +0700

    dump: miui_MERLINIDGlobal_V12.0.1.0.QJOIDXM_709d727d58_10.0.zip

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-Link="https://index.rnxyr.my.id/PixelExperience_Plus_merlin-11.0-20210713-1425-UNOFFICIAL.zip"
-FileName="$(pwd)/miui_MERLIN_V12.5.2.0.RJOCNXM_dc5c5badb0_11.0.zip"
+Link="https://index.rnxyr.my.id/miui_MERLINIDGlobal_V12.0.1.0.QJOIDXM_709d727d58_10.0.zip"
+FileName="$(pwd)/miui_MERLINIDGlobal_V12.0.1.0.QJOIDXM_709d727d58_10.0.zip"
 export DEBIAN_FRONTEND=noninteractive
 apt-get -y update && apt-get -y upgrade && apt-get install -y unace unrar zip unzip p7zip-full p7zip-rar sharutils rar uudeview mpack arj cabextract file-roller device-tree-compiler liblzma-dev python3 python3-pip brotli liblz4-tool axel gawk aria2 detox wget
 pip3 install backports.lzma protobuf pycrypto docopt
